87a30b3d6dc60362472aa326ac278e51306de699
suthakaranburaj <suthakaranburaj@gmail.com>
2024-09-22 18:53:48 +0530

    Homepage working going on!

--- client/src/pages/Homepage/HomePage.jsx
+++ client/src/pages/Homepage/HomePage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import image from "https://gratisography.com/photo/cyber-kitty/" 
+import image1 from '../../assets/video1_thumbnail.jpg'
+import image2 from '../../assets/profile_pic.webp'
 function HomePage() {
     return (
-        <div className='flex'>
-            <div>
-                <img 
-                    src={image}
-                    alt="Description of the image" 
-                />
+        <div className='flex text-white'>
+            <div className=''>
+                <img className='w-[30%] m-1 rounded-xl' src={image1} alt="" />
+                <div className='flex my-3'>
+                    <img className='w-[5%] rounded-full mx-2 ' src={image2} alt="" />
+                    <div>
+                        <p className='description'>How to Fuck with full energy -By Tushar Hasule</p>
+                        <p className='userChannel'>Tushar Hasule Talks</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
