fcfef8e06437260daf8965dfd51bc563ab424c7b
Tushar <tushar.hasule@somaiya.edu>
2024-09-22 17:44:23 +0530

    layout

--- client/src/pages/Layout/LayoutPage.js
+++ client/src/pages/Layout/LayoutPage.js
@@ -4,13 +4,21 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from '../../components/Navbar/Navbar';
 import HomePage from '../Homepage/HomePage';
 
+import './Layout.scss';
+
 function LayoutPage() {
     return (
         <>
-            <Navbar />
-            <Routes>
-                <Route path='/' element={<HomePage />} />
-            </Routes>
+            <div className='container'>
+                <div className='left-side'>
+                    <Navbar />
+                </div>
+                <div className='right-side'>
+                    <Routes>
+                        <Route path='/' element={<HomePage />} />
+                    </Routes>
+                </div>
+            </div>
         </>
     )
 }
