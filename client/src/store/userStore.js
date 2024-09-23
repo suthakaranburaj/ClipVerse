3ac0f862c18f0fad37c632541ee961259322933b
suthakaranburaj <suthakaranburaj@gmail.com>
2024-09-24 00:59:59 +0530

    login , logout zustand added

--- client/src/store/userStore.js
+++ client/src/store/userStore.js
@@ -1,10 +1,30 @@
 import { create } from "zustand";
-
-// helper function 1 create localstore user 2 delete 
+import axios from 'axios'
 
 const useStore = create((set) => ({
-    // user : null 
-    // login : () => { axios->login->response data->user }
+
+    user : null ,
+    login : async (credentials) =>{
+
+        try {
+            const response= await axios.post("/api/v1/login",credentials);
+    
+            set({ user: response.data.user});
+    
+            localStorage.setItem("user",JSON.stringify(response.data.user));
+        } catch (error) {
+            console.error("Login failed",error);
+        }
+
+    },
+
+    logout : () => {
+
+        set({user:null});
+
+        localStorage.removeItem("user");
+    },
+
 }));
 
 export default useStore;
