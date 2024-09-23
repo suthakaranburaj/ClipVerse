20dcd84de461fe9a2dc17a0fe67a59879da1ddd5
Tushar <tushar.hasule@somaiya.edu>
2024-09-24 01:12:05 +0530

    login

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import "./LoginPage.scss";
 
 function LoginPage() {
+
+    const [email, setEmail] = useState();
+    const [userName, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const submitHandler = () => {
+        
+    }
 
     return (
         <>
             <div className='card'>
                 <h1>Login to Youtube</h1>
                 <div className='input-container'>
-                    <input type="text" placeholder='Enter your username' />
-                    <input type="email" placeholder='Enter your email'/>
-                    <input type="password" placeholder='Enter your passwrod'/>
+                    <input
+                        type="text"
+                        placeholder='Enter your username'
+                        value={userName}
+                        onChange={(e) => { setUserName(e.target.value) }}
+                    />
+                    <input
+                        type="email"
+                        placeholder='Enter your email'
+                        value={email}
+                        onChange={(e) => { setEmail(e.target.value) }}
+                    />
+                    <input
+                        type="password"
+                        placeholder='Enter your passwrod'
+                        value={password}
+                        onChange={(e) => { setPassword(e.target.value) }}
+                    />
                 </div>
-                <button>Login</button>
+                <button onClick={submitHandler}>Login</button>
             </div>
         </>
     )
--- client/src/store/userStore.js
+++ client/src/store/userStore.js
@@ -1,26 +1,26 @@
 import { create } from "zustand";
-import axios from 'axios'
+import axios from 'axios';
 
 const useStore = create((set) => ({
 
-    user : null ,
-    login : async (credentials) =>{
+    user: null,
+    login: async ({ name, email, password }) => {
 
         try {
-            const response= await axios.post("/api/v1/login",credentials);
-    
-            set({ user: response.data.user});
-    
-            localStorage.setItem("user",JSON.stringify(response.data.user));
+            const response = await axios.post("/api/v1/login", { name, email, password });
+
+            set({ user: response.data.user });
+
+            localStorage.setItem("user", JSON.stringify(response.data.user));
         } catch (error) {
-            console.error("Login failed",error);
+            console.error("Login failed", error);
         }
 
     },
 
-    logout : () => {
+    logout: () => {
 
-        set({user:null});
+        set({ user: null });
 
         localStorage.removeItem("user");
     },
