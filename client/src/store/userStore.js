dcf38574bb47c15d32d40fe95b7e38b8663660ee
Tushar <tushar.hasule@somaiya.edu>
2024-09-24 00:27:28 +0530

    blueprint

--- client/src/store/userStore.js
+++ client/src/store/userStore.js
@@ -1,5 +1,10 @@
 import { create } from "zustand";
 
-const useStore = create((set) => ({}));
+// helper function 1 create localstore user 2 delete 
+
+const useStore = create((set) => ({
+    // user : null 
+    // login : () => { axios->login->response data->user }
+}));
 
 export default useStore;
