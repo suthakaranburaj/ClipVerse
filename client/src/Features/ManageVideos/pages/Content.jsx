9c1435e560a105a32158a9820f1c520337ac8182
TANMAY6767 <tanmayt36y@gmail.com>
2024-09-29 23:34:37 +0530

    content

--- client/src/Features/ManageVideos/pages/Content.jsx
+++ client/src/Features/ManageVideos/pages/Content.jsx
@@ -12,8 +12,23 @@ function Content() {
         <>
             <div className='outer'>
                 <div className='level-1'>
-
+                    <div className='head'>
+                        <div>
+                            Channel content
+                        </div>
+                        
+                    </div>
+                    <div className='bttns'>
+                        <div className='pressables'><button>Videos</button></div>
+                        <div  className='pressables'><button>Shorts</button></div>
+                        <div  className='pressables'><button>Live</button></div>
+                        <div  className='pressables'><button>Post</button></div>
+                        <div  className='pressables'><button>Playlists</button></div>
+                        <div  className='pressables'><button>Podcasts</button></div>
+                        <div  className='pressables'><button>Promotions</button></div>
+                    </div>
                 </div>
+                <div className="divider"></div>
                 <div className='level-2'>
 
                 </div>
