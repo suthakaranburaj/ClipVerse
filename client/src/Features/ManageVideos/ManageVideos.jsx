0dcfd2ac957fd4659d0e12f91e2cf19c69b23631
TANMAY6767 <tanmayt36y@gmail.com>
2024-09-29 22:51:04 +0530

    manging video in progress

--- client/src/Features/ManageVideos/ManageVideos.jsx
+++ client/src/Features/ManageVideos/ManageVideos.jsx
@@ -5,29 +5,51 @@ import Navbar from './components/NavMangeVideo/Sidebar';
 import SearchBar from './components/ManageSearchBar/SearchBar';
 import NoNavbar from './components/NavMangeVideo/NoSideBar'
 import devStore from '../../store/devStore';
+import Dashboard from './pages/Dashboard';
+import Content from './pages/Content';
 
 import './ManageVideos.scss';
 
 
 function ManageVideos() {
 
-    const { isNavOpen } = devStore();
+    const { isNavOpen, displayer } = devStore(); // Access the state
+
+    // Create a function to dynamically render components based on `displayer`
+    const renderContent = () => {
+        switch (displayer) {
+            case 'dashboard':
+                return <Dashboard/>;
+            case 'content':
+                return <Content/>;
+
+            default:
+                return <Dashboard />; // Fallback to Dashboard
+        }
+    };
+
     return (
         <>
             <div className='SearchBar'>
                 <SearchBar />
             </div>
             <div className='container'>
-                <div  className='left-side2'>
+                <div className='left-side2'>
                     {isNavOpen ? " " : <NoNavbar />}
                 </div>
-                
+
                 <div className={`left-side ${isNavOpen ? '' : 'hidden'}`}>
-                {isNavOpen ? <Navbar /> : <NoNavbar />}
+                    {isNavOpen ? <Navbar /> : <NoNavbar />}
                 </div>
                 <div className={isNavOpen ? 'right-side-WithNav' : 'right-side-WithoutNav'}>
                     <Outlet />
+                    {renderContent()}
                 </div>
+                
+                
+
+
+
             </div>
         </>
     )
