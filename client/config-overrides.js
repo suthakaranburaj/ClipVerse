349de37e13db554476bb6caaec84d30b0eec1856
suthakaranburaj <suthakaranburaj@gmail.com>
2024-10-21 15:31:27 +0530

    deployment requirements

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,7 +1,5 @@
-
-import path from 'path';
-
-export default function override(config) {
+const path = require('path');
+module.exports = function override(config) {
     config.output.path = path.resolve(__dirname, 'dist');
     return config;
-}
+};
