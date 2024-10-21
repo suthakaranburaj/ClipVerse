7643b839175a5db1ffe1b0bdd09e35a1a6fa46c2
suthakaranburaj <suthakaranburaj@gmail.com>
2024-10-21 17:13:45 +0530

    deployment changes

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://clip-verse.vercel.app'],
     credentials: true
 }))
 
