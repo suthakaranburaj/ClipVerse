7ee90ac6627a9208841c786351ef4484e1d88ad4
suthakaranburaj <suthakaranburaj@gmail.com>
2024-10-23 18:05:42 +0530

    community page responsiveness done successfully!

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,10 +7,9 @@ module.exports = {
     extend: {
       screens: {
         'custom600': {'max': '766px'}, // Custom media query
-        'custom480': {'max':'480px'},
+        'custom500': {'max':'500px'},
         'custom1024':{'max':'1024px'},
         'custom766':{'max':'766px'},
-        'custom480':{'max':'480px'},
         'custom360':{'max':'360px'},
       },
     },
