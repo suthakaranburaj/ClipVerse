2f3d892d3f87a787b55da3cbeef40c03c2f4aa86
suthakaranburaj <suthakaranburaj@gmail.com>
2024-10-24 21:00:35 +0530

    frontend url

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ app.use(cors({
     origin: 
         [
             'http://localhost:3000', 
-            'https://clipverse.onrender.com',
+            'https://clip-verse.vercel.app',
         ],
     credentials: true
 }))
